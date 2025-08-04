4d6da68b81e04ade1abc069c0c58b2bdeb192320
ErickHenrique <erick.2henriquehtp@gmail.com>
2025-08-04 20:37:14 -0300

    Fix: adjust zodytype provider

--- src/app/orders/orderRoutes.ts
+++ src/app/orders/orderRoutes.ts
@@ -1,11 +1,47 @@
 import { FastifyInstance } from 'fastify'
-import { createOrderSchema } from './orderSchema'
-import { createOrderService } from './orderService'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { createOrderRepo } from './orderRepository'
 
 export async function orderRoutes(app: FastifyInstance) {
-  app.post('/orders', async (request, reply) => {
-    const data = createOrderSchema.parse(request.body)
-    const order = await createOrderService(data)
-    reply.code(201).send({ id: order.id })
-  })
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/orders',
+    {
+      schema: {
+        summary: 'Create an order',
+        tags: ['orders'],
+        body: z.object({
+          customerId: z.number().int().positive(),
+          total: z.number().positive(),
+          products: z
+            .array(
+              z.object({
+                productId: z.number().int().positive(),
+                quantity: z.number().int().positive(),
+                price: z.number().positive(),
+              })
+            )
+            .min(1),
+        }),
+        response: {
+          201: z.object({
+            orderId: z.number(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { customerId, total, products } = request.body
+
+      const order = await createOrderRepo({
+        customerId,
+        total,
+        products,
+      })
+
+      
+
+      return reply.status(201).send({ orderId: order.id })
+    }
+  )
 }
